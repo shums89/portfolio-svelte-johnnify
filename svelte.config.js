f652bb233ab1c50aa8f123da9bc1716bdafe0dbd
Aleksandr Shumatbaev <sh889@ya.ru>
2024-08-19 13:43:35 +0500

    feat: netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,26 @@
-import adapter from '@sveltejs/adapter-auto'
+// import adapter from '@sveltejs/adapter-auto'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// compilerOptions: {
-	//   runes: true,
-	// },
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+  // compilerOptions: {
+  //   runes: true,
+  // },
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
 
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
-		alias: {
-			$m: 'src/paraglide/messages.js',
-			$paraglide: 'src/paraglide/*'
-		}
-	}
+  kit: {
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+    // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+    adapter: adapter(),
+    alias: {
+      $m: 'src/paraglide/messages.js',
+      $paraglide: 'src/paraglide/*'
+    }
+  }
 }
 
 export default config
